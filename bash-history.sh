da507fd02128fe9e318aa136239d18a024fb2738
philippetavernier <philippe.tavernier@gmail.com>
2020-01-14 17:34:05 +0100

    Create bash-history.sh

---
+++ bash-history.sh
@@ -0,0 +1,383 @@
+ python
+    2  sudo halt
+    3  sudo apt-get install http://sourceforge.net/projects/bcusdk/files/pthsem/pthsem_2.0.8.tar.gz/download
+    4  tar -xzvf pthsem_2.0.8.tar.gz 
+    5  cd pthsem-2.0.8/
+    6  ./configure -with-mctx-mth=sjlj -with-mctx-dsp=ssjlj -with-mctx-stk=sas -disable-shared
+    7  make
+    8  sudo make install
+    9  cd
+   10  wget http://www.auto.tuwien.ac.at/~mkoegler/eib/bcusdk_0.0.5.tar.gz
+   11  tar -xzvf bcusdk_0.0.5.tar.gz 
+   12  cd bcusdk-0.0.5/
+   13  ./configure -without-pth-test -enable-onlyeibd -enable-eibnetip -enable-eibnetiptunnel -enable-eibnetipserver -enable-groupcache -enable-usb
+   14  make
+   15  sudo make install
+   16  cd http://sourceforge.net/projects/bcusdk/files/pthsem/pthsem_2.0.8.tar.gz/download
+   17  cd
+   18  wget http://www.auto.tuwien.ac.at/~mkoegler/eib/bcusdk_0.0.5.tar.gz
+   19  tar -xzvf bcusdk_0.0.5.tar.gz 
+   20  cd bcusdk-0.0.5/
+   21  ./configure -without-pth-test -enable-onlyeibd -enable-eibnetip -enable-eibnetiptunnel -enable-eibnetipserver -enable-groupcache -enable-usb
+   22  make
+   23  sudo make install
+   24  eibtrace 127.0.0.1
+   25  sudo eibtrace 127.0.0.1
+   26  eibnetmux -s -t -u -e -d --pidfile=/var/run/eibnetmux.pid 192.168.0.51 
+   27  sudo apt-get install libxml2-dev liblua5.1-0-dev
+   28  cd http://sourceforge.net/projects/bcusdk/files/pthsem/pthsem_2.0.8.tar.gz/download
+   29  cd wget http://sourceforge.net/projects/bcusdk/files/pthsem/pthsem_2.0.8.tar.gz/download
+   30  cd wget
+   31  sudo cd wget http://sourceforge.net/projects/bcusdk/files/pthsem/pthsem_2.0.8.tar.gz/download
+   32  cd wget http://sourceforge.net/projects/bcusdk/files/pthsem/pthsem_2.0.8.tar.gz/download
+   33  ls
+   34  sudo halt
+   35  eibnetmux -s -t -u -e -d --pidfile=/var/run/eibnetmux.pid 192.168.0.51 
+   36  eibnetmux -s -t -u -e -d --pidfile=/var/run/eibnetmux.pid 192.168.0.51
+   37  ps aux | grep eibnetmux
+   38  root 1993 0.0 0.0 3152 912 ? Ss 17:02 0:00 eibnetmux -s -t -u -e -d --pidfile=/var/run/eibnetmux.pid 192.168.0.51
+   39  root 2067 0.0 0.0 4060 820 pts/0 S+ 17:20 0:00 grep eibnetmux eibtrace 127.0.0.1
+   40  cd wget http://downloads.sourceforge.net/project/bcusdk/pthsem/pthsem_2.0.8.tar.gz?r=&ts=1428482684&use_mirror=netcologne
+   41  cd
+   42  wget 
+   43  http://downloads.sourceforge.net/project/bcusdk/pthsem/pthsem_2.0.8.tar.gz?r=&ts=1428482684&use_mirror=netcologne
+   44  cd
+   45  wget http://downloads.sourceforge.net/project/bcusdk/pthsem/pthsem_2.0.8.tar.gz?r=&ts=1428482684&use_mirror=netcologne
+   46  cd
+   47  cd cd pthsem-2.0.8/
+   48  ./configure -with-mctx-mth=sjlj -with-mctx-dsp=ssjlj -with-mctx-stk=sas -
+   49  cd pthsem-2.0.8/
+   50  tar -xzvf pthsem_2.0.8.tar.gz 
+   51  ls
+   52  wget http://downloads.sourceforge.net/project/bcusdk/pthsem/pthsem_2.0.8.tar.gz?r=&ts=1428482684&use_mirror=netcologne
+   53  mv pthsem_2.0.8.tar.gz\?r\=.1 pthsem_2.0.8.tar.gz
+   54  tar -xzvf pthsem_2.0.8.tar.gz
+   55  cd pthsem-2.0.8/
+   56  ./configure -with-mctx-mth=sjlj -with-mctx-dsp=ssjlj -with-mctx-stk=sas -disable-shared
+   57  make
+   58  sudo make install
+   59  cd
+   60  wget http://www.auto.tuwien.ac.at/~mkoegler/eib/bcusdk_0.0.5.tar.gz
+   61  tar -xzvf bcusdk_0.0.5.tar.gz 
+   62  cd bcusdk-0.0.5/
+   63  ./configure -without-pth-test -enable-onlyeibd -enable-eibnetip -enable-eibnetiptunnel -enable-eibnetipserver -enable-groupcache -enable-usb
+   64  cd
+   65  wget http://www.auto.tuwien.ac.at/~mkoegler/eib/bcusdk_0.0.5.tar.gz
+   66  tar -xzvf bcusdk_0.0.5.tar.gz 
+   67  cd bcusdk-0.0.5/
+   68  ./configure -without-pth-test -enable-onlyeibd -enable-eibnetip -enable-eibnetiptunnel -enable-eibnetipserver -enable-groupcache -enable-usb
+   69  make
+   70  sudo findknxusb
+   71  pi@raspberrypi ~ $ sudo eibd --trace=1 -u usb:1:6:1:0:0
+   72  sudo eibd --trace=1 -u usb:1:6:1:0:0
+   73  sudo eibd -d -D -i -S -T usb:1:4:1:0:0
+   74  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+   75  findknxusb 
+   76  sudo findknxusb 
+   77  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+   78  sudo eibd --trace=1 -u usb:1:6:1:0:0
+   79  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+   80  sudo eibd --trace=1 -u usb:1:6:1:0:0
+   81  sudo reboot
+   82  groupswrite ip:192.168.2.220 0/0/1 0
+   83  groupswrite ip:127.0.0.1 0/0/1 0
+   84  sudo groupswrite ip:127.0.0.1 0/0/1 0
+   85  history
+   86  sudo groupswrite ip:127.0.0.1 0/0/1 0
+   87  sudo eibd --trace=1 -u usb:1:6:1:0:0
+   88  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+   89  sudo eibd -d -D -i -S -T -u usb:1:6:1:0:0
+   90  sudo reboot
+   91  groupswrite 
+   92  groupswrite -h
+   93  pi@raspberrypi ~ $ sudo eibd --trace=1 -u usb:1:6:1:0:0
+   94  sudo eibd --trace=1 -u usb:1:6:1:0:0
+   95  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+   96  groupswrite ip:127.0.0.1 0/0/1 0
+   97  sudo groupswrite ip:127.0.0.1 0/0/1 0
+   98  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+   99  sudo findknxusb
+  100  groupswrite ip:127.0.0.1 0/0/1 0
+  101  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+  102  sudo eibd --trace=1 -u usb:1:6:1:0:0
+  103  sudo reboot
+  104  sudo eibd --trace=1 -u usb:1:6:1:0:0
+  105  sudo findknxusb 
+  106  sudo eibd --trace=1 -u usb:1:4:1:0:0
+  107  sudo eibd -d -D -i -S -T usb:1:4:1:0:0
+  108  sudo eibd -d -D -i -S -T -u usb:1:4:1:0:0
+  109  sudo eibd --trace=1 -u usb:1:4:1:0:0
+  110  groupswrite ip:127.0.0.1 0/0/1 0
+  111  sudo eibd -d -D -i -S -T usb:1:4:1:0:0
+  112  groupswrite ip:127.0.0.1 0/0/1 0
+  113  sudo groupswrite ip:127.0.0.1 0/0/1 0
+  114  groupswrite ip:127.0.0.1 0/0/1 0
+  115  groupswrite ip:127.0.0.1 0/0/1 1
+  116  groupswrite ip:127.0.0.1 0/0/1 0
+  117  groupswrite ip:127.0.0.1 0/0/1 1
+  118  groupswrite ip:127.0.0.1 0/0/1 0
+  119  groupswrite ip:127.0.0.1 0/0/1 1
+  120  groupswrite ip:127.0.0.1 0/0/1 0
+  121  groupswrite ip:127.0.0.1 0/0/1 1
+  122  groupswrite ip:127.0.0.1 0/0/1 0
+  123  groupswrite ip:127.0.0.1 0/0/1 1
+  124  groupswrite ip:127.0.0.1 0/0/1 0
+  125  groupswrite ip:127.0.0.1 0/0/1 1
+  126  groupswrite ip:127.0.0.1 0/0/1 0
+  127  groupswrite ip:127.0.0.1 0/0/1 1
+  128  groupswrite ip:127.0.0.1 0/0/1 0
+  129  groupswrite ip:127.0.0.1 0/0/1 1
+  130  groupswrite ip:127.0.0.1 0/0/1 0
+  131  groupswrite ip:127.0.0.1 0/0/1 1
+  132  groupswrite ip:127.0.0.1 0/0/1 0
+  133  groupswrite ip:127.0.0.1 0/0/2 1
+  134  groupswrite ip:127.0.0.1 0/0/3 1
+  135  groupswrite ip:127.0.0.1 0/0/4 1
+  136  groupswrite ip:127.0.0.1 0/0/1 1
+  137  groupswrite ip:127.0.0.1 0/0/1 0
+  138  groupswrite ip:127.0.0.1 0/0/1 1
+  139  groupswrite ip:127.0.0.1 0/0/1 0
+  140  groupswrite ip:127.0.0.1 0/0/1 1
+  141  groupswrite ip:127.0.0.1 0/0/1 0
+  142  groupswrite ip:127.0.0.1 0/0/2 0
+  143  groupswrite ip:127.0.0.1 0/0/3 0
+  144  groupswrite ip:127.0.0.1 0/0/4 0
+  145  killall eibnetmux
+  146  sudo eibd --trace=1 -u usb:1:4:1:0
+  147  sudo eibd -d -D -i -S -T usb:1:4:1:0:0
+  148  groupswrite ip:127.0.0.1 0/0/1 0
+  149  groupswrite ip:127.0.0.1 0/0/1 1
+  150  groupswrite ip:127.0.0.1 0/0/1 0
+  151  groupswrite ip:127.0.0.1 0/0/1 1
+  152  groupswrite ip:127.0.0.1 0/0/1 0
+  153  groupswrite ip:127.0.0.1 0/0/1 1
+  154  groupswrite ip:127.0.0.1 0/0/1 0
+  155  groupswrite ip:127.0.0.1 0/0/1 1
+  156  groupswrite ip:127.0.0.1 0/0/1 0
+  157  groupswrite ip:127.0.0.1 0/0/1 1
+  158  groupswrite ip:127.0.0.1 0/0/1 0
+  159  sudo knxusb
+  160  sudo findknxusb 
+  161  apt-get install php5 php5-cli php5-mysql libapache2-mod-php5 php5-dev apache2 mysql-server php-pear
+  162  sudo apt-get install php5 php5-cli php5-mysql libapache2-mod-php5 php5-dev apache2 mysql-server php-pear
+  163  vi /etc/init.d/eibnetmux
+  164  sudo su 
+  165  sudo su
+  166  cd /var/www/
+  167  ls -l
+  168  nano index.html
+  169  ls -l
+  170  nano index.html
+  171  ls -l
+  172  cd ..
+  173  ls -l
+  174  sudo chown pi www/
+  175  sudo chown -R pi www/
+  176  ls -l
+  177  cd www/
+  178  ls -l
+  179  chmod +x *.*
+  180  ls -l
+  181  nano index.html
+  182  cd /var/www/
+  183  ls -l
+  184  chmod -R -x *.*
+  185  ls -l
+  186  chmod -R +x *.*
+  187  ls -l
+  188  nano index.html 
+  189  nano style.css 
+  190  ls -l
+  191  nano index.html 
+  192  nano style.css 
+  193  nano index.html 
+  194  nano style.css 
+  195  nano index.html 
+  196  nano style.css 
+  197  nano index.html 
+  198  nano style.css 
+  199  nano index.html 
+  200  nano style.css 
+  201  mv index.html index.php
+  202  nano index.php 
+  203  sudo halt
+  204  groupswrite ip:127.0.0.1 0/0/1 0
+  205  groupswrite ip:127.0.0.1
+  206  groupswrite ip:127.0.0.1 0/0/1 0
+  207  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+  208  pi@raspberrypi ~ $ groupswrite ip:127.0.0.1 0/0/1 0
+  209  groupswrite ip:127.0.0.1 0/0/1 0
+  210  groupswrite ip:127.0.0.1 0/0/1 1
+  211  groupswrite ip:127.0.0.1 0/0/1 0
+  212  groupswrite ip:127.0.0.1 0/0/1 1
+  213  cd /var/www/
+  214  nano chambreon.sh
+  215  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+  216  groupswrite ip:127.0.0.1 0/0/1 0
+  217  sudo eibd --trace=1 -u usb:1:6:1:0:0
+  218  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+  219  groupswrite ip:127.0.0.1 0/0/1 0
+  220  groupswrite ip:127.0.0.1 0/0/1 1
+  221  groupswrite ip:127.0.0.1 0/0/1 0
+  222  groupswrite ip:127.0.0.1 0/0/1 1
+  223  nano chambreon.sh
+  224  groupswrite ip:127.0.0.1 0/0/1 0
+  225  nano index.php
+  226  groupswrite ip:127.0.0.1 0/0/1 0
+  227  groupswrite ip:127.0.0.1 0/0/1 1
+  228  groupswrite ip:127.0.0.1 0/0/1 0groupswrite ip:127.0.0.1 0/0/1 0
+  229  groupswrite ip:127.0.0.1 0/0/1 0
+  230  groupswrite ip:127.0.0.1 0/0/1 1
+  231  groupswrite ip:127.0.0.1 0/0/1 0
+  232  nano index.php
+  233  chmod 777 chambreon.sh
+  234  nano index.php
+  235  cd /var/www/
+  236  nano index.php
+  237  ls
+  238  nano chambreon.sh
+  239  nano index.php
+  240  nano chambreon.sh
+  241  nano index.php
+  242  cd /var/www/
+  243  nano onoff.php
+  244  sudo nano onfff.php
+  245  sudo nano onoff.php
+  246  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+  247  groupswrite ip:127.0.0.1 0/0/1 0
+  248  groupswrite ip:127.0.0.1 0/0/1 1
+  249  groupswrite ip:127.0.0.1 0/0/1 0
+  250  groupswrite ip:127.0.0.1 0/0/1 1
+  251  groupswrite ip:127.0.0.1 0/0/1 0
+  252  nano onoff.php
+  253  sudo nano jquery.min.js
+  254  cd /var/www/
+  255  sudo nano jquery.min.js 
+  256  cd /var/www/
+  257  nano index.php
+  258  ls-
+  259  ls
+  260  nano jquery.min.js.save
+  261  sudo nano jquery.min.js
+  262  cd /var/www/
+  263  ls
+  264  sudo nano jquery.min.js.save
+  265  rm jquery.min.js.save
+  266  ls
+  267  rm jquery.min.js.save.1
+  268  ls
+  269  nano jquery.min.js
+  270  cd /var/www/
+  271  ls
+  272  nano jquery.min.js
+  273  cd /var/www/
+  274  ls
+  275  rm jquery.min.sj.save
+  276  yes
+  277  cd /var/www/
+  278  ls
+  279  rm jquery.min.js.save
+  280  rm jquery.min.js.save.1
+  281  ls
+  282  sudo nano jquery.min.js
+  283  cd /var/www/
+  284  ls
+  285  nano jquery.min.js.save
+  286  nano jquery.min.js
+  287  cd /var/www/
+  288  ls
+  289  wget http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+  290  ls
+  291  cat jquery.min.js
+  292  ls
+  293  nano onoff.php 
+  294  sudo nano onoff.php 
+  295  sudo nano command1.php 
+  296  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+  297  groupswrite ip:127.0.0.1 0/0/1 0
+  298  Send requestgroupswrite ip:127.0.0.1 0/0/1 1
+  299  groupswrite ip:127.0.0.1 0/0/1 0
+  300  groupswrite ip:127.0.0.1 0/0/1 1
+  301  nano command1.php 
+  302  sudo nano comand1.php
+  303  sudo nano command1.php 
+  304  sudo nano onoff.php 
+  305  groupswrite ip:127.0.0.1 0/0/1 1
+  306  groupswrite ip:127.0.0.1 0/0/1 0
+  307  nano /var/www/onoff.php 
+  308  nano /var/www/command1.php 
+  309  groupswrite ip:127.0.0.1 0/0/1 1
+  310  groupswrite ip:127.0.0.1 0/0/1 0
+  311  nano /var/www/command1.php 
+  312  nano /var/www/command1.php
+  313  nano /var/www/command1.php 
+  314  cd /var/www
+  315  nano command1.php
+  316  nano command1.php 
+  317  groupswrite ip:127.0.0.1 0/0/1 1
+  318  groupswrite ip:127.0.0.1 0/0/1 0
+  319  nano command1.php 
+  320  nano onoff.php
+  321  nano command1.php 
+  322  nano command2.php
+  323  nano onoff.php 
+  324  nano command2.php
+  325  nano onoff.php
+  326  nano onff.php
+  327  nano command2.php
+  328  nano onoff.php
+  329  nano command2.php
+  330  nano onoff.php
+  331  nano onoff.php 
+  332  nano onoff.php
+  333  nano onoff.php 
+  334  nano onoff.php
+  335  nano command2.php
+  336  nano command3.php
+  337  nano onoff.php
+  338  nano command4.php
+  339  nano onoff.php
+  340  nano index.php
+  341  nano onoff.php
+  342  nano index.php
+  343  sudo chmod 777 /dev/ttyUSB0
+  344  history
+  345  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+  346  groupswrite ip:127.0.0.1 0/0/1 1
+  347  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+  348  sudo eibd -d -D -i -S -T usb:1:4:1:0:0
+  349  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+  350  groupswrite ip:127.0.0.1 0/0/1 1
+  351  sudo ssh pi@192.168.2.220
+  352  groupswrite ip:127.0.0.1 0/0/1 1
+  353  sudo eibd -d -D -i -S -T usb:1:6:1:0:0
+  354  nano /var/www/temp.html
+  355  nano /var/www/temp.php
+  356  nano /var/www/temp.txt
+  357  mv /var/www/temp.php /var/www/a.php
+  358  nano /var/www/temp.html
+  359  echo '1' > /dev/ttyACM0 
+  360  ls -l /dev/ttyACM0 
+  361  ifconfig
+  362  cat /etc/rc.local 
+  363  echo '1' > /dev/ttyACM0 
+  364  stty -F /dev/ttyUSB1 cs8 9600 ignbrk -brkint -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts 
+  365  stty -F /dev/ttyACM0 cs8 9600 ignbrk -brkint -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts 
+  366  tail -f /dev/ttyACM0 &
+  367  echo '1' > /dev/ttyACM0 
+  368  ls /var/www/
+  369  nano /var/www/bouton.php
+  370  cat /var/www/bouton.php 
+  371  nano /var/www/thermostat.php
+  372  rm /var/www/thermostat.php
+  373  nano /var/www/thermostat.php
+  374  ls
+  375  cd /var/www/
+  376  lq
+  377  ls
+  378  nano index.php 
+  379  exit
+  380  history
+  381  sudo halt
+  382  historry
+  383  history
